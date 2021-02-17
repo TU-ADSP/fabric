042bfdb3e488d604d413d1e8b2f6841dd7c65cff
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-02-17 23:49:58 +0100

    log NewPeerClientFromEnv serverrootca as string

--- internal/peer/common/peerclient.go
+++ internal/peer/common/peerclient.go
@@ -31,7 +31,7 @@ func NewPeerClientFromEnv() (*PeerClient, error) {
 	log.Println("NewPeerClientFromEnv: address: ", address)
 	log.Println("NewPeerClientFromEnv: override: ", override)
 	log.Printf("NewPeerClientFromEnv: clientConfig: %+v\n", clientConfig)
-	log.Println("NewPeerClientFromEnv: theCert: ", clientConfig.SecOpts.ServerRootCAs[0])
+	log.Println("NewPeerClientFromEnv: theCert: ", string(clientConfig.SecOpts.ServerRootCAs[0]))
 	if err != nil {
 		return nil, errors.WithMessage(err, "failed to load config for PeerClient")
 	}

62d172e9f2016fc8fdfe0fdd638cccced3adfdbc
Tobias Petrich <tobias.petrich@campus.tu-berlin.de>
2021-02-17 23:09:43 +0100

    log InitCmdFactory peerAddresses %v

--- internal/peer/chaincode/common.go
+++ internal/peer/chaincode/common.go
@@ -459,9 +459,9 @@ func InitCmdFactory(cmdName string, isEndorserRequired, isOrdererRequired bool, 
 		if err = validatePeerConnectionParameters(cmdName); err != nil {
 			return nil, errors.WithMessage(err, "error validating peer connection parameters")
 		}
-		log.Println("InitCmdFactory: peerAddresses: ", peerAddresses)
+		log.Printf("InitCmdFactory: peerAddresses: %v\n", peerAddresses)
 		for i, address := range peerAddresses {
-			log.Println("InitCmdFactory: peerAddress: ", address)
+			log.Printf("InitCmdFactory: peerAddress: %v\n", address)
 			var tlsRootCertFile string
 			if tlsRootCertFiles != nil {
 				tlsRootCertFile = tlsRootCertFiles[i]
